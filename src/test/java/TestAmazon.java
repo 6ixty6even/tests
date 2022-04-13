a7bc6a295725f9033395eb33d01032df68da4926
Serhiy <serhiy@Vadims-MacBook-Pro-2.local>
2022-04-13 14:33:31 +0300

    ngfdngfdnj

--- src/test/java/TestAmazon.java
+++ src/test/java/TestAmazon.java
@@ -1,16 +1,29 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class TestChrome{
+public class TestAmazon {
 
 @Test
     public void runGoodle(){
     WebDriverManager.chromedriver().setup();
     WebDriver driver = new ChromeDriver();
     driver.get("https://www.google.com.ua/");
-    driver.findElement(By.xpath("//input[@class='gLFyf gsfi']")).sendKeys("youtube");
+    driver.findElement(By.xpath("//input[@class='gLFyf gsfi']")).sendKeys("amazon\n");
+    driver.findElements(By.xpath("//div[@class='tF2Cxc']")).get(0).click();
+    JavascriptExecutor jse = (JavascriptExecutor)driver;
+    jse.executeScript("scrollBy(0,250)");
+    driver.findElement(By.xpath("//img[@alt='Electronics']")).click();
     }
 }
+
+
+
+
+
+
+
+   //h3[@class="LC20lb MBeuO DKV0Md"]
