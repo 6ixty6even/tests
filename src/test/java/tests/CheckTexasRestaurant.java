115103988c8a7155f70f184a77bdcd19c587df69
Serhiy <serhiy@Vadims-MacBook-Pro-2.local>
2022-04-27 17:12:13 +0300

    Clean

--- src/test/java/tests/CheckTexasRestaurant.java
+++ src/test/java/tests/CheckTexasRestaurant.java
@@ -16,28 +16,6 @@ public class CheckTexasRestaurant extends TestInit {
         sleep(3);
         Assert.assertTrue(homePage.texasRestauran().isDisplayed());
     }
-
-    @Test
-    public void newTestTexasRestaurant() {
-        goToEatStreet();
-        HomePage homePage = new HomePage(driver);
-        homePage.getSearchField().sendKeys("Texas City, TX, USA");
-        sleep(2);
-        homePage.getClickOnTexas().click();
-        sleep(3);
-        Assert.assertTrue(homePage.texasRestauran().isDisplayed());
-    }
-
-    @Test
-    public void newnewTestTexasRestaurant() {
-        goToEatStreet();
-        HomePage homePage = new HomePage(driver);
-        homePage.getSearchField().sendKeys("Texas City, TX, USA");
-        sleep(2);
-        homePage.getClickOnTexas().click();
-        sleep(3);
-        Assert.assertTrue(homePage.texasRestauran().isDisplayed());
-    }
 }
 
 
