2d1a515d00238b476310c46ec019e317240cd18f
6ixty6even <1077stargonkiff7@gmail.com>
2022-04-27 19:31:15 +0300

    clean2

--- src/test/java/tests/TestAmazon.java
+++ src/test/java/tests/TestAmazon.java
@@ -1,26 +1,16 @@
-package tests;
-
-import org.testng.annotations.Test;
-
-public class TestAmazon extends TestInit {
-
-    @Test
-    public void runGoodle() {
+//package tests;
+//
+//import org.testng.annotations.Test;
+//
+//public class TestAmazon extends TestInit {
+//
+//    @Test
+//    public void runGoodle() {
 //        WebDriverManager.chromedriver().setup();
 //        WebDriver driver = new ChromeDriver();
 //        driver.get("https://www.google.com.ua/");
 //        driver.findElement(By.xpath("//input[@class='gLFyf gsfi']")).sendKeys("amazon\n");
 //        driver.findElements(By.xpath("//div[@class='tF2Cxc']")).get(0).click();
 //        driver.findElement(By.xpath("//img[@alt='Electronics']")).click();
-
 //    }
-
-
-// test 1 vs 2 who better
-//
-//    @Test
-//    public void TestAmazon2() {
-//      goToGoogle();
-//      getSearchField().sendKeys("amazon\n");
-    }
-}
+//}
